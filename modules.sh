511182dd605d640871608f3e190fdf5882e8b8b8
SUDO <82466939+ItsMeSudo@users.noreply.github.com>
2022-07-05 21:19:55 +0200

    Delete modules.sh

--- modules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SUDO                 2022              SERVERHU.EU
-
-modules=$@
-cd /data && \
-npm install $modules && \
-echo "A kiválasztott modulok telepítése sikeres volt." && \
-/bin/bash
