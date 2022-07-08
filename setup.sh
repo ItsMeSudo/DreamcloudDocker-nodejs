32602bae9e85ecabc850a3efa86f82a13b2bc1dd
SUDO <82466939+ItsMeSudo@users.noreply.github.com>
2022-07-08 12:44:25 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #SUDO                 2022              SERVERHU.EU
 
 cd /root && \
-    apt install unzip wget git curl -y && \
+    apt install unzip wget git curl ffmpeg youtube-dl -y && \
     cd /root/DreamcloudDocker-nodejs && \
     bash nodesetup.sh && \
     apt install nodejs gcc g++ make -y && \
