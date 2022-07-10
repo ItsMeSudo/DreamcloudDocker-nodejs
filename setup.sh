1b8a75009cf52a33b9aed758c574871187f6800e
SUDO <82466939+ItsMeSudo@users.noreply.github.com>
2022-07-10 20:49:53 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,10 +5,14 @@ cd /root && \
     apt install unzip wget git curl ffmpeg youtube-dl -y && \
     cd /root/DreamcloudDocker-nodejs && \
     bash nodesetup.sh && \
-    apt install nodejs gcc g++ make -y && \
+    apt install nodejs gcc g++ make autoconf automake libtool -y && \
+    sed -i 's/\r//' /root/DreamcloudDocker-nodejs/gitclone && \
     mv /root/DreamcloudDocker-nodejs/gitclone /usr/bin/ && \
+    sed -i 's/\r//' /root/DreamcloudDocker-nodejs/container-start && \
     mv /root/DreamcloudDocker-nodejs/container-start /usr/bin/ && \
+    sed -i 's/\r//' /root/DreamcloudDocker-nodejs/modules-i && \
     mv /root/DreamcloudDocker-nodejs/modules-i /usr/bin/ && \
     sed -i 's/\r//' /root/DreamcloudDocker-nodejs/setservertoken && \
     mv /root/DreamcloudDocker-nodejs/setservertoken /usr/bin/ && \
+    sed -i 's/\r//' /root/DreamcloudDocker-nodejs/nodestart && \
     mv /root/DreamcloudDocker-nodejs/nodestart /usr/bin/
